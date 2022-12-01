f62ed24d754d47bcced8aa833bee75ee22bf2b84
Erki Liepkalns <erki.liepkalns@gmail.com>
2022-12-02 00:51:38 +0200

    Day 1 commit

---
+++ day1/part1.ps1
@@ -0,0 +1,31 @@
+﻿$DB = Get-Content "day1.txt"
+$var1=0
+$var2=0
+$array2=@([int])
+foreach ($line in $DB)
+{
+    $integer=[int]$line
+    if ($integer -gt 0)
+    {
+        $var1=$integer+$var1
+    }
+    else
+    {
+        $array2 += $var1
+        $var1=[int]0
+    }   
+}
+Write-Host "------------------ Part 1 ------------------"
+Write-Host "Elf carriying the most calories:"
+Write-Host ($array2 | Sort-Object -descending | Select-Object -Skip 1 -First 1)
+Write-Host "------------------ Part 2 ------------------"
+Write-Host "Top 3 elves:"
+$top3=($array2 | Sort-Object -descending | Select-Object -Skip 1 -First 3)
+Write-Host $top3
+
+foreach ($amount in $top3)
+{
+    $var2=[int]$amount+$var2
+}
+Write-Host "And they are carriying a total of (calories):"
+$var2
