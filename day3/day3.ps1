4d15f970f33839aef45ae81bff08500b55a52114
Erki Liepkalns <erki.liepkalns@gmail.com>
2022-12-04 01:10:12 +0200

    day 3 part 2 done

--- day3/day3.ps1
+++ day3/day3.ps1
@@ -1,4 +1,3 @@
-#$DB=(Get-Content .\day3\example.txt)
 $DB=(Get-Content .\day3\input.txt)
 $priority=0
 $total=0
@@ -14,10 +13,8 @@ function part1()
         $bp_half=($backpack.Length)/2 #counts string length, divides into 2
         $1half=$backpack.Substring(0,$bp_half) #returns with 0-half of the string
         $2half=$backpack.Substring($bp_half) #returns with the value starting from the second half of string
-        #$1half #prints out 1st half (for debug)
-        #$2half #prints out 2nd half (for debug)
         $contents= ($1half.ToCharArray()) | Where-Object {($2half.ToCharArray()) -ccontains $_}| Select-Object -first 1 #last part might be sketchy - might miss out on MORE than 1 matches per backpack.
-        #$contents
+        #https://stackoverflow.com/questions/3494115/what-does-mean-in-powershell
         $priority=(calc($contents))
         $total=$total+$priority
         Write-Host "Priority calculated in total: $total"
@@ -36,12 +33,12 @@ while ($c -lt ($DB.length)) #cycles until end of array
         $check1=($DB[$a].ToCharArray()) | Where-Object {($DB[$b].ToCharArray()) -ccontains $_}
         $check2=($DB[$b].ToCharArray()) | Where-Object {($DB[$c].ToCharArray()) -ccontains $_}
 
-        $per3= $check1 | Where-Object {$check2 -ccontains $_} | Select-Object -first 1
+        $per3= $check1 | Where-Object {$check2 -ccontains $_} | Select-Object -first 1 # same idea as line 19
         $2prio=(calc($per3))
         Write-Host "calculated priority for group: $2prio"
         $2total=$2total+(calc($per3))
         write-host "Total calculated (for groups) so far: $2total"
-        $a=$a+3
+        $a=$a+3 #move the selected array values forward by 3
         $b=$b+3
         $c=$c+3
     }
