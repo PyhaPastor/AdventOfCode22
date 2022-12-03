2b87f6f5d2d177c583037d775595de84accaf7b0
Erki Liepkalns <erki.liepkalns@gmail.com>
2022-12-04 01:05:04 +0200

    finished with part2

--- day3/day3.ps1
+++ day3/day3.ps1
@@ -3,31 +3,60 @@ $DB=(Get-Content .\day3\input.txt)
 $priority=0
 $total=0
 
-foreach ($backpack in $DB)
+#Call function according to part of task ('part1' or 'part2')
+#part1
+part2
+function part1()
 {
-#$backpack="CrZsJsPPZsGzwwsLwLmpwMDw" #for testing purposes
-$bp_half=($backpack.Length)/2 #counts string length, divides into 2
-$1half=$backpack.Substring(0,$bp_half) #returns with 0-half of the string
-$2half=$backpack.Substring($bp_half) #returns with the value starting from the second half of string
-#$1half #prints out 1st half (for debug)
-#$2half #prints out 2nd half (for debug)
-$contents= ($1half.ToCharArray()) | Where-Object {($2half.ToCharArray()) -ccontains $_}| Select-Object -first 1 #last part might be sketchy - might miss out on MORE matches.
-#$contents
-
-
-#$value=[byte][char]"$contents" #fails when UPPERCASE
-
-#calculate priority:
-
-if($contents -cmatch "[a-z]")
+    foreach ($backpack in $DB)
     {
-    $priority=([byte][char]"$contents")-96
+        #$backpack="CrZsJsPPZsGzwwsLwLmpwMDw" #for testing purposes
+        $bp_half=($backpack.Length)/2 #counts string length, divides into 2
+        $1half=$backpack.Substring(0,$bp_half) #returns with 0-half of the string
+        $2half=$backpack.Substring($bp_half) #returns with the value starting from the second half of string
+        #$1half #prints out 1st half (for debug)
+        #$2half #prints out 2nd half (for debug)
+        $contents= ($1half.ToCharArray()) | Where-Object {($2half.ToCharArray()) -ccontains $_}| Select-Object -first 1 #last part might be sketchy - might miss out on MORE than 1 matches per backpack.
+        #$contents
+        $priority=(calc($contents))
+        $total=$total+$priority
+        Write-Host "Priority calculated in total: $total"
     }
-    if($contents -cmatch "[A-Z]")
-    {
-    $priority=([byte][char]"$contents")-38
-    }
-
-$total=$priority+$total
 }
-Write-Host "Priority calculated in total: $total"
+
+function part2()
+{
+$2total=0
+$a=0
+$b=1
+$c=2
+
+while ($c -lt ($DB.length)) #cycles until end of array
+    {
+        $check1=($DB[$a].ToCharArray()) | Where-Object {($DB[$b].ToCharArray()) -ccontains $_}
+        $check2=($DB[$b].ToCharArray()) | Where-Object {($DB[$c].ToCharArray()) -ccontains $_}
+
+        $per3= $check1 | Where-Object {$check2 -ccontains $_} | Select-Object -first 1
+        $2prio=(calc($per3))
+        Write-Host "calculated priority for group: $2prio"
+        $2total=$2total+(calc($per3))
+        write-host "Total calculated (for groups) so far: $2total"
+        $a=$a+3
+        $b=$b+3
+        $c=$c+3
+    }
+}
+
+
+function calc($letter,$result) #Calculates Priority value for letter
+{
+    if($letter -cmatch "[a-z]")
+    {
+        $result=([byte][char]"$letter")-96
+    }
+        if($letter -cmatch "[A-Z]")
+    {
+        $result=([byte][char]"$letter")-38
+    }
+    return $result
+}
